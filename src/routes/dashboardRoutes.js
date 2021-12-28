986b0958aed2ed8138b9b42cc3fa5340716cd7e4
eldhose <eldho1998@gmail.com>
2021-12-28 22:04:04 +0530

    updated.

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -14,6 +14,12 @@ function router(nav) {
     }, {
         text: "Add Authors",
         link: "/dashboard/addauthors"
+    }, {
+        text: "Delete Books",
+        link: "/dashboard/deletebooks"
+    }, {
+        text: "Delete Authors",
+        link: "/dashboard/deleteauthors"
     }];
 
     dashboardRouter.get("/", function(req, res) {
@@ -25,6 +31,7 @@ function router(nav) {
     });
 
     dashboardRouter.post("/addbook", function(req, res) {
+
         var item = {
             title: req.body.title,
             author: req.body.author,
@@ -37,6 +44,34 @@ function router(nav) {
         res.redirect('/dashboard/addbooks');
 
     });
+
+    dashboardRouter.post("/deletebook", function(req, res) {
+        bookdata.findByIdAndRemove({ _id: req.body.id })
+            .then(function() {
+                res.redirect('/dashboard/deletebooks?' + req.body.id);
+            })
+    });
+    dashboardRouter.post("/deleteauthor", function(req, res) {
+        authordata.findByIdAndRemove({ _id: req.body.id })
+            .then(function() {
+                res.redirect('/dashboard/deleteauthors?' + req.body.id);
+            })
+    });
+
+    dashboardRouter.post("/updatebook", function(req, res) {
+        var item = {
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+            image: req.body.image,
+            description: req.body.description
+        }
+        var book = bookdata(item);
+        book.update({ _id: req.body.id });
+        res.redirect('/dashboard/updatebooks?' + req.body.id);
+    });
+
+
 
     dashboardRouter.post("/addauthor", function(req, res) {
         var item = {
@@ -64,6 +99,36 @@ function router(nav) {
                 description: "You can add new authors here.",
                 nav,
             });
+        } else if (id == "deletebooks") {
+            bookdata.find()
+                .then(function(books) {
+                    res.render("deletebooks", {
+                        title: "Delete Books",
+                        description: "Delete books of the Library App",
+                        nav,
+                        books
+                    });
+                })
+        } else if (id == "deleteauthors") {
+            authordata.find()
+                .then(function(authors) {
+                    res.render("deleteauthors", {
+                        title: "Delete Authors",
+                        description: "Delete authors of the books in the Library App",
+                        nav,
+                        authors
+                    });
+                })
+        } else if (id == "updatebooks") {
+            bookdata.findOne({ _id: req.query.id })
+                .then(function(books) {
+                    res.render("updatebooks", {
+                        title: "Update Book",
+                        description: "Update book: " + req.query.id,
+                        nav,
+                        books
+                    });
+                })
         }
     });
 
