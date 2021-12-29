03e42c3b0f4e5aa7c52d16ec32e198959091fbdc
eldhose <eldho1998@gmail.com>
2021-12-30 00:11:16 +0530

    updated with remote database.

--- src/models/authordata.js
+++ src/models/authordata.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/library');
+mongoose.connect('mongodb://u32rnsmc4mg8rr3hd9dz:yrysHnzaKAkOJ4SIzPor@bbecwzcrcyjkosq-mongodb.services.clever-cloud.com:27017/bbecwzcrcyjkosq');
+//mongoose.connect('mongodb://localhost:27017/library');
+//mongoose.connect('mongodb://username:password@host:port/database');
 
 const Schema = mongoose.Schema;
 const AuthorSchema = new Schema({
